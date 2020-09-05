a5dd4b558b2937ddfbda54671bfad4a25828b188
swapnilsardeshmukh <54372416+swapnilsardeshmukh@users.noreply.github.com>
2020-09-06 01:15:38 +0530

    adding constant with different methods

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 //adding constant hello swapnil and same with paragraph
-//
+//with back ticks 
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name="Swapnil";
-const num=6;
+const fname = "Swapnil";
+const lname = "Sardeshmukh";
+const num = 7;
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}</h1>
-    <p>Your lucky number is {num}</p>
+    <h1>
+      Hello {`${fname} ${lname}`} and your favno is {num}
+    </h1>
+    <p>But Your lucky number for now is {Math.round(Math.random() * 10)}</p>
+    <p> Thank you</p>
+    <p> {fname + "" +lname}</p>
   </div>,
   document.getElementById("root")
 );
