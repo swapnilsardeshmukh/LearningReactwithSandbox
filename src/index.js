9b785d82c17f7c34f7fd9e46e369124e9bd8fb70
swapnilsardeshmukh <54372416+swapnilsardeshmukh@users.noreply.github.com>
2020-09-06 01:00:26 +0530

    learning  constant in react

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const name="Swapnil";
-const num=7;
+const num=6;
 
 ReactDOM.render(
   <div>
     <h1>Hello {name}</h1>
-    <p>Your lucky number is 6</p>
+    <p>Your lucky number is {num}</p>
   </div>,
   document.getElementById("root")
 );
