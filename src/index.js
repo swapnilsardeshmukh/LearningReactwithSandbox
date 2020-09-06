fe977c27684b1ba7f627fd9ce591436297ad45e2
swapnilsardeshmukh <54372416+swapnilsardeshmukh@users.noreply.github.com>
2020-09-07 01:04:05 +0530

    Styling task with inline css

--- src/index.js
+++ src/index.js
@@ -1,38 +1,47 @@
-//adding className
-//changimg javascript to jsx
-//contentEditable
-//spellCheck
-//fix image length
-//image const
+//Create a React app from scratch.
+//Show a single h1 that says "Good morning" if between midnight and 12PM.
+//or "Good Afternoon" if between 12PM and 6PM.
+//or "Good evening" if between 6PM and midnight.
+//Apply the "heading" style in the styles.css
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
 
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQpOjPb6hODqD7dr8cMii_VeBEaJPVtanlSJQ&usqp=CAU";
+const m = new Date(2020, 1, 1, 7);
+//<h1>{w}</h1>
+let greeting = "";
+
+let hours = "";
+
+const customStyle = {
+  color: ""
+};
+
+if (m.getHours() >= 3 && m.getHours() <= 12) {
+  greeting = "Good Morning";
+  hours = m.getHours();
+  customStyle.color = "red";
+} else if (m.getHours() > 12 && m.getHours() <= 18) {
+  greeting = "Good Afternoon";
+  hours = m.getHours();
+  customStyle.color = "green";
+} else if (m.getHours() > 18 && m.getHours() <= 21) {
+  greeting = "Good Evening";
+  hours = m.getHours();
+  customStyle.color = "brown";
+} else {
+  greeting = "Good Night";
+  hours = m.getHours();
+  customStyle.color = "blue";
+}
+
 ReactDOM.render(
   <div>
-    <h1 className="heading" contentEditable="true" spellCheck="false">
-      My Favourite Foods
+    <h1 className="heading" style={customStyle}>
+      {greeting} its {hours} in 24hrs Clock{" "}
     </h1>
-    <div>
-      <img className="flowers" src={name} />
-      <img
-        className="flowers"
-        alt="flower"
-        src="https://images.unsplash.com/photo-1526047932273-341f2a7631f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-      ></img>
-      <img
-        className="flowers"
-        alt="flower"
-        src="https://content.thriveglobal.com/wp-content/uploads/2018/11/Flower.jpg"
-      ></img>
-      <img
-        className="flowers"
-        alt="flower"
-        src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/diy-paper-flowers-1582662788.jpg?crop=0.631xw:0.945xh;0.0128xw,0.0504xh&resize=640:*"
-      ></img>
-    </div>
   </div>,
   document.getElementById("root")
 );
