efe5ec387fd6a4cf657491655ec1b1f4df1e8380
swapnilsardeshmukh <54372416+swapnilsardeshmukh@users.noreply.github.com>
2020-09-08 14:24:18 +0530

    learning import export part2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import pi, { doublePI, triplePI } from "./math";
+// '*' wildcard is not recommended import what is needed
+//import * as pi from "./math.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ul>
+    <li>{pi}</li>
+    <li>{doublePI()}</li>
+    <li>{triplePI()}</li>
+    <li>{pi / pi}</li>
+  </ul>,
+  document.getElementById("root")
+);
+
+// ReactDOM.render(
+//   <ul>
+//     <li>{pi.default}</li>
+//     <li>{pi.doublePI()}</li>
+//     <li>{pi.triplePI()}</li>
+
+//   </ul>,
+//   document.getElementById("root")
+// );
