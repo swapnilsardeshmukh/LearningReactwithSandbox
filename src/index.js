e5bb17150e9d75cd30f33585ec4972aaff4bef91
swapnilsardeshmukh <54372416+swapnilsardeshmukh@users.noreply.github.com>
2020-09-08 12:06:52 +0530

    Learning React Components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Heading from "./Heading";
-import List from "./List";
+import Heading from "./Components/Heading";
+import List from "./Components/List";
 
 ReactDOM.render(
   <div>
