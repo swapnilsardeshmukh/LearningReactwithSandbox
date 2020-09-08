3a72d8c84f0660e10b74c718d2a7facfd0a23e89
swapnilsardeshmukh <54372416+swapnilsardeshmukh@users.noreply.github.com>
2020-09-08 15:02:40 +0530

    import export challenge1

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import pi, { doublePI, triplePI } from "./math";
-// '*' wildcard is not recommended import what is needed
-//import * as pi from "./math.js";
+import { add, multiply, subtract, divide } from "./calculator";
+
+//2nd method
+import * as calc from "./calculator";
+//Import the add, multiply, subtract and divide functions
+//from the calculator.js file.
+//If successful, your website should look the same as the Final.png
 
 ReactDOM.render(
   <ul>
-    <li>{pi}</li>
-    <li>{doublePI()}</li>
-    <li>{triplePI()}</li>
-    <li>{pi / pi}</li>
+    <li>{add(1, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{subtract(7, 2)}</li>
+    <li>{divide(5, 2)}</li>
   </ul>,
   document.getElementById("root")
 );
-
-// ReactDOM.render(
-//   <ul>
-//     <li>{pi.default}</li>
-//     <li>{pi.doublePI()}</li>
-//     <li>{pi.triplePI()}</li>
-
-//   </ul>,
-//   document.getElementById("root")
-// );
+///////////////////2nd method with wildcard//
+//This will give final o/p
+ReactDOM.render(
+  <ul>
+    <li>{calc.add(1, 2)}</li>
+    <li>{calc.multiply(2, 3)}</li>
+    <li>{calc.subtract(7, 2)}</li>
+    <li>{calc.divide(5, 2)}</li>
+  </ul>,
+  document.getElementById("root")
+);
